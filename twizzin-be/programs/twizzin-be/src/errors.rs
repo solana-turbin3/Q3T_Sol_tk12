99de0bbb2938ec0b3ee8a26f22f331535c755450
Tim Knapp <timknapp12@gmail.com>
2024-08-29 19:23:35 -0600

    Squashed 'capstone/twizzin/' changes from ddbf5e5..8158d21
    8158d21 Merge pull request #7 from timknapp12/add-player
    bed9c7d new anchor build
    e6be943 remove comments
    41accd9 add player
    922b2eb hash answers and store hash in game account
    e713d15 Add Player struct
    495fa80 update Game account
    d5abbbf Merge pull request #6 from timknapp12/create-be
    318b7c1 set up init_game fn
    f35b469 add erros
    11de7e1 init anchor project
    485b92c add backend files to .gitignore
    c4b5460 Merge pull request #5 from timknapp12/admin-signin
    f513329 add question groups
    e9dd88b add TextArea
    31ec8ad add new colors
    6ec7eb7 add react date picker
    7ca5172 update to supbase sign in
    89d6582 add AppContext hook
    54dd048 Merge pull request #4 from timknapp12/env-variables
    1edd950 add create game route
    699c24f add supabase and env variables
    e4f07c5 add pink to colors
    fc550a5 update favicon
    
    git-subtree-dir: capstone/twizzin
    git-subtree-split: 8158d21bd9f657a03629c97e39e447f4728d8277

---
+++ twizzin-be/programs/twizzin-be/src/errors.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+
+pub enum ErrorCode {
+    #[msg("Submitted before the game start time")]
+    BeforeStartTime,
+    #[msg("Submitted after the game end time")]
+    AfterEndTime,
+    #[msg("Player already in game")]
+    PlayerAlreadyInGame,
+    #[msg("Player not in game")]
+    PlayerNotInGame,
+    #[msg("Name must be between 1 and 32 characters")]
+    NameTooLong,
+}
---
+++ twizzin-be/programs/twizzin-be/src/lib.rs
@@ -0,0 +1,48 @@
+use anchor_lang::prelude::*;
+
+declare_id!("8uqVcDqy6CSS9Ss8VYpPWD5J8daQy3BvsAecQ64DgFAR");
+
+pub mod errors;
+pub use errors::ErrorCode;
+
+pub mod contexts;
+pub use contexts::*;
+
+pub mod state;
+pub use state::*;
+
+pub mod utils;
+
+#[program]
+pub mod twizzin_be {
+    use super::*;
+
+    pub fn init_game(
+        ctx: Context<InitGame>,
+        name: String,
+        entry_fee: u64,
+        commission: u16,
+        start_time: u64,
+        end_time: u64,
+        answers: Vec<(u8, String, String)>,
+    ) -> Result<()> {
+        ctx.accounts.init_game(
+            name, entry_fee, commission, start_time, end_time, answers, &ctx.bumps,
+        )
+    }
+
+    pub fn add_player(ctx: Context<AddPlayer>) -> Result<()> {
+        ctx.accounts.add_player()
+    }
+
+    // join_game by player
+    // submit guesses by player
+    // end_game by escrow? - how do I trigger this?
+    // identify_winners
+    // calculate_payout
+    // calculate_treasury
+    // pay_winners
+    // pay_treasury
+    // close accounts (vault, escrow, players, game state)? close all of these?
+    // who do we payout when closing accounts? Is it cheap and dirty to pay the treasury?
+}
