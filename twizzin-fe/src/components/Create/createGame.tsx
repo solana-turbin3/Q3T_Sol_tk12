373cac8c00c3dbef338b1ca8eb05755396c3f691
Tim Knapp <timknapp12@gmail.com>
2024-09-26 22:05:41 -0600

    Squashed 'capstone/twizzin/' changes from e0ef236..4a23c49
    4a23c49 Merge pull request #21 from timknapp12/create-game-fe
    4d47296 fix params error
    95fb01c add questions to context
    29504ba add translation strings for current ui
    10758ec reorder pages to handle translations
    dcbb053 add translation config
    4fd9560 add type folder
    53b8506 add types
    e745424 add admin page
    7455e7c Merge pull request #20 from timknapp12/deploy-to-devnet
    392a2c2 add program id to readme
    5efeb3d add devnet cluster
    80a46cf Merge pull request #19 from timknapp12/update-landing-page
    049599d add google anlx
    78b4169 Merge pull request #18 from timknapp12/update-landing-page
    b5ebdc7 add google anlx
    feb6f11 Merge pull request #17 from timknapp12/update-landing-page
    39eabea update more info section with better info
    3ead241 step 4 to step 5
    f520306 Merge pull request #16 from timknapp12/ui-designs-for-pitch-deck
    b339b81 update landing page to new logo and text
    fbc23a4 add label to input
    619d70b add inputs
    df430ea Merge pull request #15 from timknapp12/update-lannding-page
    1afbea2 update landing page
    
    git-subtree-dir: capstone/twizzin
    git-subtree-split: 4a23c4988c4612dd1bdc82fd4253c1b3edec64b7

--- twizzin-fe/src/components/Create/createGame.tsx
+++ twizzin-fe/src/components/Create/createGame.tsx
@@ -4,9 +4,12 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import QuestionGroup from './questionGroup';
 import { FaPlus } from 'react-icons/fa6';
+import { useTranslation } from 'react-i18next';
 
 const CreateGame = () => {
   const { gameTitle, setGameTitle, gameTime, setGameTime } = useAppContext();
+
+  const { t } = useTranslation();
 
   const handleDateChange = (date: Date | null) => {
     setGameTime(date);
@@ -15,34 +18,69 @@ const CreateGame = () => {
   return (
     <Column className='w-full h-full flex-grow gap-12' justify='between'>
       <Column className='w-full gap-4'>
-        <p className='text-2xl font-bold'>Create a Twizzin game</p>
+        <p className='text-2xl font-bold'>{t('Create a Twizzin game')}</p>
         <Row className='w-full gap-4'>
           <Input
-            style={{ width: '50%' }}
+            style={{ width: '48%' }}
             type='text'
             value={gameTitle}
             onChange={(e: { target: { value: string } }) =>
               setGameTitle(e.target.value)
             }
-            placeholder='Game Title'
+            placeholder={t('Game Title')}
+            label={t('Game Title')}
           />
-          <DatePicker
+          <Input
+            style={{ width: '48%' }}
+            type='text'
+            value={''}
+            onChange={(e: { target: { value: string } }) =>
+              setGameTitle(e.target.value)
+            }
+            placeholder={t('Entry Fee in SOL')}
+            label={t('Entry Fee in SOL')}
+          />
+          <Input
+            style={{ width: '48%' }}
+            type='text'
+            value={''}
+            onChange={(e: { target: { value: string } }) =>
+              setGameTitle(e.target.value)
+            }
+            placeholder={t('Commission in %')}
+            label={t('Commission in %')}
+          />
+          <Input
+            style={{ width: '48%' }}
+            type='text'
+            value={''}
+            onChange={(e: { target: { value: string } }) =>
+              setGameTitle(e.target.value)
+            }
+            placeholder={t('Number of Max Winners')}
+            label={t('Number of Max Winners')}
+          />
+          {/* <DatePicker
             selected={gameTime}
             onChange={handleDateChange}
             showTimeSelect
             className='w-full px-4 py-2 border border-lightPurple rounded-md focus:outline-none focus:ring-2 focus:ring-darkPurple focus:border-transparent bg-light-background dark:bg-dark-background'
             placeholderText='Select date and time'
             dateFormat='Pp'
-          />
+          /> */}
         </Row>
-        <div className='h-10' />
+        <div className='h-4' />
         <Column className='w-full gap-6'>
           <QuestionGroup index={0} />
           <QuestionGroup index={1} />
         </Column>
       </Column>
       <Row className='w-full p-4' justify='end'>
-        <FaPlus className='cursor-pointer text-blue' size={28} />
+        <FaPlus
+          title={t('Add question')}
+          className='cursor-pointer text-blue'
+          size={28}
+        />
       </Row>
     </Column>
   );
--- twizzin-fe/src/components/Home/Landing.tsx
+++ twizzin-fe/src/components/Home/Landing.tsx
@@ -1,26 +1,37 @@
-import { FaXTwitter } from 'react-icons/fa6';
+import { FaXTwitter, FaTelegram } from 'react-icons/fa6';
 import { Column, Row } from '../containers';
-import { H3, H3Brand } from '../texts';
+import { H3 } from '../texts';
+import { useTranslation } from 'react-i18next';
 
 export const Landing = () => {
+  const { t } = useTranslation();
   return (
     <Column className='gap-4'>
       <Row className='text-center'>
-        <Column className='items-center'>
-          <H3Brand className='mt-2 mb-2'>TWIZZIN</H3Brand>
+        <Column className='items-center mt-6'>
           <H3>
-            is currently in development. Follow us on X to stay updated on our
-            progress.
+            {t(
+              'Twizzin is currently in development. Follow us on X and Telegram to stay updated on our progress.'
+            )}
           </H3>
         </Column>
       </Row>
-      <a
-        href='https://x.com/twizzinapp'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <FaXTwitter size={48} style={{ cursor: 'pointer' }} />
-      </a>
+      <Row className='gap-8'>
+        <a
+          href='https://x.com/twizzinapp'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaXTwitter size={48} style={{ cursor: 'pointer' }} />
+        </a>
+        <a
+          href='https://t.me/+Xp_4og3g-dIwNWZh'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaTelegram size={48} style={{ cursor: 'pointer' }} />
+        </a>
+      </Row>
     </Column>
   );
 };
